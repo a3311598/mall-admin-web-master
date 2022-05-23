307339427c40f5429a700512da32fb9ca56a46b5
GAVX <a3311598@qq.com>
2022-05-23 19:49:30 +0800

    修改网关端口

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
 //  BASE_API: '"https://admin-api.macrozheng.com"'
-  BASE_API: '"http://	172.19.0.7:8201/mall-admin"'
+  BASE_API: '"http://	172.19.0.1:8201/mall-admin"'
 //    BASE_API: '"http://192.168.122.1:80"'
 }
